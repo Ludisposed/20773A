992e2ceb826513a5f50b9709436717f980d49f13
Ludisposed <pjgvanschaik@gmail.com>
2018-05-07 18:51:51 +0200

    Delete Lab05.R

--- Labs/Lab05.R
+++
@@ -1,4 +0,0 @@
-setwd("C:\\Users\\Pieter.vanSchaik\\Desktop\\Microsoft Learning\\20773\\Allfiles\\Labfiles\\Lab05")
-
-flightData <- rxImport("FlightDelayData.xdf")
-clust <- rxKmeans(~ as.numeric(CRSDepTime) + Delay ,data = flightData, numClusters = 5)
